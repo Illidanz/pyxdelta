c66ee77d5568e6cb8a29ce54e1a07960d2d03ed0
Illidan <illidanrulez@gmail.com>
2023-10-05 10:43:27 +0200

    Build wheels for python 3.12

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ SOURCES = ["pyxdelta.c"]
 
 def main():
     setup(name="pyxdelta",
-          version="0.1.2",
+          version="0.1.3",
           author="Illidan",
           description="Python interface for xdelta.",
           long_description=long_description,
